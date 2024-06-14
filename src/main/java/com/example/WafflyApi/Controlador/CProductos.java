6cd73d708cad3e32334e2809b7869a53a7e54af5
mondasa <126993103+mondasa@users.noreply.github.com>
2024-06-14 17:59:21 -0600

    Update CProductos.java
    Agregar anotacion cors

--- src/main/java/com/example/WafflyApi/Controlador/CProductos.java
+++ src/main/java/com/example/WafflyApi/Controlador/CProductos.java
@@ -15,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/Productos")
 public class CProductos {
     @Autowired
